2ab2f91cef3a2fcfc615186165ad2f5912684432
kajarosz <karolinaannajarosz@gmail.com>
2022-02-25 11:11:17 +0100

    added character jumping keyboard command

--- characters.py
+++ characters.py
@@ -8,15 +8,19 @@ class Hero(object):
                  pygame.image.load('images/lama_R3.png'), pygame.image.load('images/lama_R4.png')]
     walk_left = [pygame.image.load('images/lama_L1.png'), pygame.image.load('images/lama_L2.png'),
                  pygame.image.load('images/lama_L3.png'), pygame.image.load('images/lama_L4.png')]
+    stand = pygame.image.load('images/lama_F1.png')
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, width):
         self.x = x
         self.y = y
+        self.width = width
         self.speed = 5
         self.right = True
         self.left = False
         self.standing = True
         self.walk_count = 0
+        self.jumping = False
+        self.jump_height = 10
 
     def walk(self, window):
         if not self.standing:
@@ -29,10 +33,7 @@ class Hero(object):
                 window.blit(self.walk_left[self.walk_count // 4], (self.x, self.y))
                 self.walk_count += 1
         else:
-            if self.right:
-                window.blit(self.walk_right[0], (self.x, self.y))
-            if self.left:
-                window.blit(self.walk_left[0], (self.x, self.y))
+            window.blit(self.stand, (self.x, self.y))
 
     def draw(self, window):
         self.walk(window)
--- game.py
+++ game.py
@@ -15,7 +15,7 @@ pygame.display.set_caption("Savage Lama Game")
 clock = pygame.time.Clock()
 
 # Instantiate characters and projectiles
-lama = Hero(200, 580)
+lama = Hero(200, 580, 50)
 
 # Redraw window function
 
@@ -39,18 +39,33 @@ while run:
     keys = pygame.key.get_pressed()
 
     # Move the character
-    if keys[pygame.K_RIGHT]:
+
+    if keys[pygame.K_RIGHT] and lama.x + lama.width + lama.speed < window_width:
         lama.x += lama.speed
         lama.right = True
         lama.left = False
         lama.standing = False
-    elif keys[pygame.K_LEFT]:
+    elif keys[pygame.K_LEFT] and lama.x - lama.speed > 0:
         lama.x -= lama.speed
         lama.right = False
         lama.left = True
         lama.standing = False
     else:
         lama.standing = True
+
+    if keys[pygame.K_UP]:
+        lama.jumping = True
+
+    if lama.jumping:
+        if lama.jump_height >= 0:
+            lama.y -= (lama.jump_height ** 2) * 0.5
+            lama.jump_height -= 1
+        elif -10 <= lama.jump_height < 0:
+            lama.y += (lama.jump_height ** 2) * 0.5
+            lama.jump_height -= 1
+        else:
+            lama.jumping = False
+            lama.jump_height = 10
 
     redraw_window()
 
